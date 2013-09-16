46324bf263f7a141909c8bafc5402facb1083290
Ondrej Tuma <mcbig@zeropage.cz>
2013-09-16 19:08:39 +0200

    Python package with setup.py

---
+++ poorwsgi/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name                = "poorwsgi",
+    version             = "1.0",
+    description         = "Poor WSGI connector for Python",
+    author              = "Ondrej Tuma",
+    author_email        = "mcbig@zeropage.cz",
+    url                 = "http://poorhttp.zeropage.cz/",
+    packages            = ['poorwsgi'],
+)

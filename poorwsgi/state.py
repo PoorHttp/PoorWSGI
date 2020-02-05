03489b68d6cb4672ae093ca8c2ea2deffc3d4abc
Ondrej Tuma <mcbig@zeropage.cz>
2020-02-05 09:23:42 +0100

    http_state and default decorator fix to chain usage

--- poorwsgi/state.py
+++ poorwsgi/state.py
@@ -3,8 +3,8 @@
 from operator import itemgetter
 
 __author__ = "Ondrej Tuma (McBig) <mcbig@zeropage.cz>"
-__date__ = "21 Jan 2020"
-__version__ = "2.1.1"       # https://www.python.org/dev/peps/pep-0386/
+__date__ = "5 Feb 2020"
+__version__ = "2.2.0dev"       # https://www.python.org/dev/peps/pep-0386/
 
 DECLINED = 0
 

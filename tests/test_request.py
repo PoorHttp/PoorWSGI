6ab9546b7327b40c731dc9571e69285c2f98661c
Ondrej Tuma <mcbig@zeropage.cz>
2020-06-09 10:13:33 +0200

    input classes getlist method have default argument
    * Default None arguments fixes

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,29 +1,77 @@
 from io import BytesIO
 
-from poorwsgi.request import JsonDict, JsonList, parse_json_request
+from poorwsgi.request import JsonDict, JsonList, parse_json_request, \
+    EmptyForm, Args, FieldStorage
+
+
+class TestEmpty:
+    def test_emptry_form(self):
+        form = EmptyForm()
+        assert form.getvalue("name") is None
+        assert form.getvalue("name", "PooWSGI") == "PooWSGI"
+        assert form.getfirst("name") is None
+        assert form.getfirst("age", "23", int) == 23
+        assert tuple(form.getlist("values", ("3", "4"), int)) == (3, 4)
+        assert tuple(form.getlist("values")) == ()
 
 
 class TestJSON:
     def test_json_dict(self):
         json = JsonDict(age=23, items=[1, 2], size="25")
+        assert json.getvalue("no") is None
         assert json.getvalue("name", "PooWSGI") == "PooWSGI"
         assert json.getvalue("age") == 23
+        assert json.getfirst("no") is None
         assert json.getfirst("age") == "23"
         assert json.getfirst("items") == "1"
         assert tuple(json.getlist("items", fce=str)) == ("1", "2")
+        assert tuple(json.getlist("values", ("3", "4"), int)) == (3, 4)
+        assert tuple(json.getlist("values")) == ()
 
     def test_json_list_empty(self):
         json = JsonList()
+        assert json.getvalue("no") is None
         assert json.getvalue("name", "PooWSGI") == "PooWSGI"
         assert json.getvalue("age", 23) == 23
+        assert json.getfirst("no") is None
         assert json.getfirst("age", 23) == "23"
         assert json.getfirst("name", "2", int) == 2
+        assert tuple(json.getlist("ages", ["1", "2"], int)) == (1, 2)
+        assert tuple(json.getlist("ages")) == ()
 
     def test_json_list(self):
         json = JsonList([1, 2])
         assert json.getvalue("age") == 1
         assert json.getfirst("age") == "1"
         assert tuple(json.getlist("items", fce=str)) == ("1", "2")
+
+
+class TestArgs:
+    class Req:
+        environ = {}
+
+    def test_empty(self):
+        args = Args(self.Req())
+        assert args.getvalue("no") is None
+        assert args.getvalue("name", "PooWSGI") == "PooWSGI"
+        assert args.getfirst("no") is None
+        assert args.getfirst("age", "23", int) == 23
+        assert tuple(args.getlist("values", ("3", "4"), int)) == (3, 4)
+        assert tuple(args.getlist("values")) == ()
+
+
+class TestForm:
+    class Req:
+        environ = {}
+
+    def test_empty(self):
+        form = FieldStorage(self.Req())
+        assert form.getvalue("no") is None
+        assert form.getvalue("name", "PooWSGI") == "PooWSGI"
+        assert form.getfirst("no") is None
+        assert form.getfirst("age", "23", int) == 23
+        assert tuple(form.getlist("values", ("3", "4"), int)) == (3, 4)
+        assert tuple(form.getlist("values")) == ()
 
 
 class TestParseJson:

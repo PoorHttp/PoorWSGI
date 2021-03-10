b42d175509597af42b7919a71bcf96df72ea5199
Ondrej Tuma <mcbig@zeropage.cz>
2021-03-10 12:37:52 +0100

    bug fix release

--- poorwsgi/state.py
+++ poorwsgi/state.py
@@ -3,7 +3,7 @@
 from operator import itemgetter
 
 __author__ = "Ondrej Tuma (McBig) <mcbig@zeropage.cz>"
-__date__ = "9 Mar 2021"
+__date__ = "10 Mar 2021"
 __version__ = "2.3.3"       # https://www.python.org/dev/peps/pep-0386/
 
 DECLINED = 0

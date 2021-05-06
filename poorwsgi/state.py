ca4fa7f6ceeec149d60612a1b4ac25742b289b95
Ondrej Tuma <mcbig@zeropage.cz>
2021-05-06 15:39:41 +0200

    fix develop version

--- poorwsgi/state.py
+++ poorwsgi/state.py
@@ -4,7 +4,7 @@ from operator import itemgetter
 
 __author__ = "Ondrej Tuma (McBig) <mcbig@zeropage.cz>"
 __date__ = "12 Mar 2021"
-__version__ = "2.3.4"       # https://www.python.org/dev/peps/pep-0386/
+__version__ = "2.4.0.dev0"       # https://www.python.org/dev/peps/pep-0386/
 
 DECLINED = 0
 

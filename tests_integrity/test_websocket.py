5ee93b0f5a8598ee970a2b9344e25fdc9b1b4711
Ondrej Tuma <mcbig@zeropage.cz>
2023-11-29 13:16:45 +0100

    websocket test

---
+++ tests_integrity/test_websocket.py
@@ -0,0 +1,64 @@
+"""HTTP Digest example test."""
+from os import environ
+from os.path import dirname, join, pardir
+from uuid import uuid1
+from base64 import encodebytes
+
+from pytest import fixture
+# websocket-client
+from websocket import WebSocket  # type: ignore
+
+from . support import start_server, check_url
+
+# pylint: disable=inconsistent-return-statements
+# pylint: disable=missing-function-docstring
+# pylint: disable=redefined-outer-name
+# pylint: disable=no-self-use
+
+
+@fixture(scope="module")
+def server(request):
+    value = environ.get("TEST_SIMPLE_URL", "").strip('/')
+    if value:
+        return value
+
+    process = start_server(
+        request,
+        join(dirname(__file__), pardir, 'examples/websocket.py'))
+
+    yield "localhost:8080"  # server is running
+    process.kill()
+    process.wait()
+
+
+@fixture(scope="module")
+def http_url(server):
+    return f"http://{server}"
+
+
+@fixture(scope="module")
+def ws_url(server):
+    return f"ws://{server}"
+
+
+class TestWebSocket:
+    """Test for WebSocket example."""
+
+    def test_upgrade(self, http_url):
+        uuid = uuid1().bytes
+        check_url(http_url+"/ws", status_code=101,
+                  headers={"Connection": "Upgrade",
+                           "Upgrade": "websocket",
+                           "Sec-WebSocket-Version": "13",
+                           "Sec-WebSocket-Key":
+                           encodebytes(uuid).decode().strip()})
+
+    def test_websocket(self, ws_url):
+        wsck = WebSocket()
+        wsck.connect(ws_url+"/ws")
+        msg = wsck.recv()
+        assert msg.endswith("Hello")
+        wsck.send("Test")
+        msg = wsck.recv()
+        assert msg.endswith("<b>Test</b>")
+        wsck.close()
